59f5c47d4e9617ade63ad4d02db46ba6c4033291
Shivam Sen <shivamsen9644@gmail.com>
2026-05-02 21:20:51 -0700

    fixed lucide-react

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://ai-travel-planner-u0kk.onrender.com/api",
 });
 
 // Attach token in every request
